6cff2a1d59e0d2127cd047e0f0bea90c0f5e2b3f
Sheshagiri-RVBD <smallipedhi@riverbed.com>
2018-08-06 19:34:18 +0530

    revert main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello from a NodeJS application.\ne-mail: msheshagirirao@gmail.com\nHi");
+  response.end("Hello from a NodeJS application.\ne-mail: msheshagirirao@gmail.com");
 });
 
 // listen on localhost:8000
